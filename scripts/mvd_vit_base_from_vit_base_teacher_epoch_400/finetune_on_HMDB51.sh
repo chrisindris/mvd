d2ddb6a86dc6f78bae887be8ab1fc15974c11e0d
root <chrisindris@gmail.com>
2023-08-24 17:21:57 -0400

    saving before restart; venv_mvd pip freeze, finetune_on_HMDB51.sh

--- scripts/mvd_vit_base_from_vit_base_teacher_epoch_400/finetune_on_HMDB51.sh
+++ scripts/mvd_vit_base_from_vit_base_teacher_epoch_400/finetune_on_HMDB51.sh
@@ -2,7 +2,7 @@
 GPUS=`nvidia-smi -L | wc -l`
 OUTPUT_DIR='OUTPUT/mvd_vit_base_with_vit_base_teacher_k400_epoch_400/finetune_on_HMDB51'
 MODEL_PATH='/data/i5O/pretrained/mvd_b_from_b_ckpt_399.pth'
-DATA_PATH='/data/i5O/kinetics-dataset/annotations'
+DATA_PATH='/data/i5O/HMDB51/annotations'
 DATA_ROOT='/data/i5O/HMDB51/hmdb51_org'
 
 MASTER_ADDR=127.0.0.1
@@ -24,7 +24,7 @@ OMP_NUM_THREADS=1 python -m torch.distributed.launch --nproc_per_node=${GPUS} \
     --output_dir ${OUTPUT_DIR} \
     --input_size 320 --short_side_size 320 \
     --opt adamw --opt_betas 0.9 0.999 --weight_decay 0.05 \
-    --batch_size 3 --update_freq 2 --num_sample 2 \
+    --batch_size 1 --update_freq 2 --num_sample 2 \
     --save_ckpt_freq 1 \
     --start_epoch 0 \
     --num_frames 16 --sampling_rate 4 \
